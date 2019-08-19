efebb3a99ae67fdb07e746927a5993eb5824a501
Leo <lbajdull@gmail.com>
2019-08-19 23:56:29 +0300

    change names of imports

--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import * as Yup from 'yup';
 import { Formik } from 'formik';
 import { Form, Button } from 'react-bootstrap';
-import HelperFunctions from '../utils/HelperFunctions';
+import CommunicationService from '../utils/CommunicationService';
 
 const schema = Yup.object().shape({
   firstName: Yup.string()
@@ -23,7 +23,7 @@ const schema = Yup.object().shape({
 });
 
 const submitRegistration = async (values: any) => {
-  await HelperFunctions.fetchFunc('POST', values, 'api/users');
+  await CommunicationService.fetchFunc('POST', values, 'api/users');
 };
 
 export function RegistrationForm() {
@@ -51,75 +51,75 @@ export function RegistrationForm() {
         isValid,
         errors
       }) => (
-        <div className='flexCenter'>
-          <div className='registrationForm'>
+        <div className="flexCenter">
+          <div className="registrationForm">
             <Form noValidate onSubmit={handleSubmit}>
-              <Form.Group controlId='formGroupfirst_name'>
+              <Form.Group controlId="formGroupfirst_name">
                 <Form.Label>First Name</Form.Label>
                 <Form.Control
-                  name='firstName'
-                  type='text'
-                  placeholder='Enter your first name'
+                  name="firstName"
+                  type="text"
+                  placeholder="Enter your first name"
                   value={values.firstName}
                   onChange={handleChange}
                   isValid={touched.firstName && !errors.firstName}
                   isInvalid={!!errors.firstName}
                 />
-                <Form.Control.Feedback type='invalid'>
+                <Form.Control.Feedback type="invalid">
                   {errors.firstName}
                 </Form.Control.Feedback>
               </Form.Group>
-              <Form.Group controlId='formGroupLastName'>
+              <Form.Group controlId="formGroupLastName">
                 <Form.Label>Last Name</Form.Label>
                 <Form.Control
-                  name='lastName'
-                  type='text'
-                  placeholder='Enter your last name'
+                  name="lastName"
+                  type="text"
+                  placeholder="Enter your last name"
                   value={values.lastName}
                   onChange={handleChange}
                   isValid={touched.lastName && !errors.lastName}
                   isInvalid={!!errors.lastName}
                 />
-                <Form.Control.Feedback type='invalid'>
+                <Form.Control.Feedback type="invalid">
                   {errors.lastName}
                 </Form.Control.Feedback>
               </Form.Group>
-              <Form.Group controlId='formGroupEmail'>
+              <Form.Group controlId="formGroupEmail">
                 <Form.Label>Email</Form.Label>
                 <Form.Control
-                  name='email'
-                  type='email'
-                  placeholder='Enter your email'
+                  name="email"
+                  type="email"
+                  placeholder="Enter your email"
                   value={values.email}
                   onChange={handleChange}
                   isValid={touched.email && !errors.email}
                   isInvalid={!!errors.email}
                 />
-                <Form.Control.Feedback type='invalid'>
+                <Form.Control.Feedback type="invalid">
                   {errors.email}
                 </Form.Control.Feedback>
               </Form.Group>
-              <Form.Group controlId='formGroupPassword'>
+              <Form.Group controlId="formGroupPassword">
                 <Form.Label>Password</Form.Label>
                 <Form.Control
-                  name='password'
-                  type='password'
-                  placeholder='Enter your password'
+                  name="password"
+                  type="password"
+                  placeholder="Enter your password"
                   value={values.password}
                   onChange={handleChange}
                   isValid={touched.password && !errors.password}
                   isInvalid={!!errors.password}
                 />
-                <Form.Control.Feedback type='invalid'>
+                <Form.Control.Feedback type="invalid">
                   {errors.password}
                 </Form.Control.Feedback>
               </Form.Group>
-              <Form.Group controlId='formGroupConfirmation'>
+              <Form.Group controlId="formGroupConfirmation">
                 <Form.Label>Password Confirmation</Form.Label>
                 <Form.Control
-                  name='passwordConfirmation'
-                  type='password'
-                  placeholder='Enter your password again'
+                  name="passwordConfirmation"
+                  type="password"
+                  placeholder="Enter your password again"
                   value={values.passwordConfirmation}
                   onChange={handleChange}
                   isValid={
@@ -127,13 +127,17 @@ export function RegistrationForm() {
                   }
                   isInvalid={!!errors.passwordConfirmation}
                 />
-                <Form.Control.Feedback type='invalid'>
+                <Form.Control.Feedback type="invalid">
                   {errors.passwordConfirmation}
                 </Form.Control.Feedback>
               </Form.Group>
 
-              <div className='flexCenter'>
-                <Button variant='primary' type='submit'>
+              <div className="flexCenter">
+                <Button
+                  variant="primary"
+                  type="submit"
+                  // onClick={submitRegistration}
+                >
                   Submit
                 </Button>
               </div>
