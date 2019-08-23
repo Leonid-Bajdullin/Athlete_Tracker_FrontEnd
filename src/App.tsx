45f4d1a28096491ac97609a9aa66d918124d3b23
unknown <lbajdull@gmail.com>
2019-08-23 19:36:28 +0300

    add some components for testing if app works

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import { Provider } from 'mobx-react';
+
 import './App.css';
+import { UserStore } from './stores/UserStore';
 import MainPage from './pages/mainPage/MainPage';
+import { UserProfile } from './components/UserProfile/UserProfile';
+import { UserExample } from './components/UserExample';
+import { BaseService } from './services/BaseService';
+
+const userStore = new UserStore(BaseService);
 
 const App: React.FC = () => {
   return (
-    <div>
-      <MainPage />
-    </div>
+    <Provider store={userStore}>
+      <div>
+        <MainPage />
+        <UserProfile />
+        <UserExample />
+      </div>
+    </Provider>
   );
 };
 
