daf44ad13427f1c1c501a689c838cbfa488ef100
unknown <lbajdull@gmail.com>
2019-08-23 19:38:12 +0300

    change method from static to DI

--- src/services/BaseService.ts
+++ src/services/BaseService.ts
@@ -1,21 +1,22 @@
 // prettier-ignore
+import {Service} from 'typedi';
 
-class BaseService {
-   public static fetchFunc (method: string, route: string, data?:{}) {
-      let obj = {
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        method: method,
-        // body: ""
-      } as any
-      if (method === "POST") {
-        obj.body = JSON.stringify(data)
-      }
-      return fetch(`http://localhost:4000/${route}`, obj)
-        .then(res => res.json())
+@Service()
+export class BaseService {
+  public fetchFunc(method: string, route: string, data?: {}) {
+    let obj = {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      method: method
+      // body: ""
+    } as any;
+    if (method === 'POST') {
+      obj.body = JSON.stringify(data);
+    }
+    return fetch(`http://localhost:4000/${route}`, obj).then((res) =>
+      res.json()
+    );
   }
 }
-
-export default BaseService;
