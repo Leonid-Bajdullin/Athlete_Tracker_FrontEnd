aade983e9f3e72e597710e898d5e490866b4bfd9
Leo <lbajdull@gmail.com>
2019-08-27 01:20:05 +0300

    add some components for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,45 @@
 import React from 'react';
 import { Provider } from 'mobx-react';
+import { Container } from 'typedi';
 
 import './App.css';
-import { UserStore } from './stores/UserStore';
+import { RootStore } from './stores/RootStore';
 import MainPage from './pages/mainPage/MainPage';
 import { UserProfile } from './components/UserProfile/UserProfile';
 import { UserExample } from './components/UserExample';
-import { BaseService } from './services/BaseService';
+import { UserService } from './services/UserService';
+import { ItemCard } from './components/ItemCard/ItemCard';
 
-const userStore = new UserStore(BaseService);
+const rootStore = new RootStore(Container.get(UserService));
 
 const App: React.FC = () => {
   return (
-    <Provider store={userStore}>
+    <Provider store={rootStore}>
       <div>
         <MainPage />
         <UserProfile />
         <UserExample />
+        <ItemCard
+          title="mark ruffalo"
+          photoUrl=""
+          id="2"
+          info="description"
+          position="headcoach"
+        />
+        <ItemCard
+          title="mark ruffalo"
+          photoUrl=""
+          id="2"
+          info="description"
+          position="athlete"
+        />
+        <ItemCard
+          title="mark ruffalo"
+          photoUrl=""
+          id="2"
+          info="description"
+          position="coach"
+        />
       </div>
     </Provider>
   );
