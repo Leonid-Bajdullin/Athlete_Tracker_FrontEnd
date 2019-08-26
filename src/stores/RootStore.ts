86c4adb464561ad7dbe1b15b6d3ad7c8ed2e5df5
Leo <lbajdull@gmail.com>
2019-08-27 01:19:11 +0300

    add one general store

--- src/stores/RootStore.ts
+++ src/stores/RootStore.ts
@@ -1,7 +1,31 @@
-import { UserStore } from './UserStore';
+import { observable, action } from 'mobx';
+
+import { UserService } from '../services/UserService';
 
 export class RootStore {
-  constructor() {
-    this.userStore = new UserStore(this);
-  }
+  constructor(private userService: UserService) {}
+
+  @observable currentUser: any = {
+    firstName: 'Leo',
+    lastName: 'Peo',
+    email: 'lbajdull@gmail.com',
+    password: 'free2play',
+    nickname: 'Adanax'
+  };
+
+  @observable token: string = '';
+
+  @observable allUsers: any = [];
+
+  @action loginUser = async (values: any) => {
+    const result = await this.userService.submitLogin(values);
+    this.currentUser = result.user;
+    this.token = result.token;
+  };
+
+  @action getAllTeams = async () => {
+    const users = await this.userService.fetchFunc('GET', 'api/teams');
+    this.allUsers = users;
+    return users;
+  };
 }
