74ecb4684e9dd598503338cbf5e81ff7dc0d6492
Leo <lbajdull@gmail.com>
2019-08-28 00:40:46 +0300

    add header with photo and desc to TeamProfile

--- src/pages/teamProfile/TeamProfile.tsx
+++ src/pages/teamProfile/TeamProfile.tsx
@@ -3,6 +3,8 @@ import { UserCard } from '../../components/UserCard/UserCard';
 import { TeamService } from '../../services/TeamService';
 import Container from 'typedi';
 
+import './TeamProfile.css';
+
 export class TeamProfile extends Component<
   { id: string },
   { teamInfo: any; membersList: Array<any> }
@@ -36,10 +38,16 @@ export class TeamProfile extends Component<
   render() {
     return (
       <div>
-        <header>
-          <img></img>
-          <div>{}</div>
-        </header>
+        <header className="team-profile-header">Team Profile</header>
+        <div className="team-profile-top">
+          <div className="team-profile-photo">
+            <img src={this.state.teamInfo.photoUrl}></img>
+          </div>
+          <section className="team-profile-description">
+            <h4>Description:</h4>
+            <div>{this.state.teamInfo.description}</div>
+          </section>
+        </div>
         <main>
           {this.state.membersList.map((item: any) => (
             <UserCard
