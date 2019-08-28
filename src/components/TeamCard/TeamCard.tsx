b02f7205dd25c68a8620fab8cc067610a56dad3c
Leo <lbajdull@gmail.com>
2019-08-29 01:09:12 +0300

    cosmetic changes

--- src/components/TeamCard/TeamCard.tsx
+++ src/components/TeamCard/TeamCard.tsx
@@ -10,15 +10,15 @@ export function TeamCard(props: {
   memberCount?: number;
 }) {
   return (
-    <section style={{ width: '15rem', height: '20rem' }} className='team-card'>
-      <div className='team-photo'>
-        <img alt='Team Photo' src={props.photoUrl} />
+    <section style={{ width: '15rem', height: '20rem' }} className="team-card">
+      <div className="team-photo">
+        <img alt="Team Photo" src={props.photoUrl} />
       </div>
-      <div className='team-info'>
-        <div className='title'>{props.name}</div>
+      <div className="team-info">
+        <div className="title">{props.name}</div>
         {/* <div>{props.id}</div> */}
-        <div className='description'>{props.memberCount}</div>
-        <Button className='button-div'>View</Button>
+        <div className="description">{props.memberCount}</div>
+        <Button className="button-div">View</Button>
       </div>
     </section>
   );
