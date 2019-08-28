1dbe4dbe815ded8bbf44f606fdf5a4e4079f0921
Leo <lbajdull@gmail.com>
2019-08-29 01:10:09 +0300

    clear initial allTeamsList value

--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -25,12 +25,12 @@ export class MainPage extends Component<
 
     this.state = {
       allTeamsList: [
-        {
-          name: 'avengers',
-          photoUrl: avengers,
-          id: '1',
-          memberCount: 11
-        }
+        // {
+        //   name: 'avengers',
+        //   photoUrl: avengers,
+        //   id: '1',
+        //   memberCount: 11
+        // }
       ]
     };
   }
@@ -44,8 +44,8 @@ export class MainPage extends Component<
     let authorisationSection;
     if (this.props.store.isLoggedIn) {
       authorisationSection = (
-        <div className='profile-tab'>
-          <div className='greeting-tab'>
+        <div className="profile-tab">
+          <div className="greeting-tab">
             Hello, {this.props.store.currentUser.firstName}
           </div>
           <UserProfile />
@@ -60,19 +60,18 @@ export class MainPage extends Component<
     }
 
     return (
-      <div className='mainpage-container'>
-        <div className='content-wrap'>
-          <header className='header'>
-            <nav className='search-input'>
+      <div className="mainpage-container">
+        <div className="content-wrap">
+          <header className="header">
+            <nav className="search-input">
               <input />
             </nav>
-            <div className='app-title'>ATHLETE TRACKER</div>
+            <div className="app-title">ATHLETE TRACKER</div>
             {authorisationSection}
           </header>
-          <main className='teamcards-list-container'>
+          <main className="teamcards-list-container">
             <h2>All teams: {this.state.allTeamsList.length}</h2>
-            <div>{`allteamslist: ${this.state.allTeamsList}`}</div>
-            <div className='all-teamcards-list'>
+            <div className="all-teamcards-list">
               {this.state.allTeamsList.map(
                 (item: {
                   name: string;
@@ -80,18 +79,23 @@ export class MainPage extends Component<
                   id: string;
                   userTeams: Array<any>;
                 }) => (
-                  <TeamCard
-                    name={item.name}
-                    photoUrl={item.photoUrl}
-                    id={item.id}
-                    // memberCount={item.userTeams.length}
-                  />
+                  <>
+                    <TeamCard
+                      name={item.name}
+                      photoUrl={item.photoUrl}
+                      id={item.id}
+                      memberCount={
+                        item.userTeams ? item.userTeams.length : undefined
+                      }
+                    />
+                    {/* <div>{item.userTeams}</div> */}
+                  </>
                 )
               )}
             </div>
           </main>
         </div>
-        <footer className='footer'>Footer</footer>
+        <footer className="footer">Footer</footer>
       </div>
     );
   }
