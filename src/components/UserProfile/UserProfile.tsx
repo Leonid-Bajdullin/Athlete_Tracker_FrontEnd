6265f706ea356f3b6acd5025d7a08c321eda5c9d
Leo <lbajdull@gmail.com>
2019-08-29 01:09:33 +0300

    make modal close after submit

--- src/components/UserProfile/UserProfile.tsx
+++ src/components/UserProfile/UserProfile.tsx
@@ -59,6 +59,7 @@ export class UserProfile extends Component<{ store?: any }, { show: boolean }> {
       this.props.store.currentUser.id,
       newValues
     );
+    this.handleClose();
   };
 
   render() {
@@ -81,11 +82,11 @@ export class UserProfile extends Component<{ store?: any }, { show: boolean }> {
 
     return (
       <>
-        <Button variant='info' onClick={this.handleShow}>
+        <Button variant="info" onClick={this.handleShow}>
           Profile page
         </Button>
 
-        <Modal size='xl' show={this.state.show} onHide={this.handleClose}>
+        <Modal size="xl" show={this.state.show} onHide={this.handleClose}>
           <Modal.Header closeButton>
             <Modal.Title>Edit User</Modal.Title>
           </Modal.Header>
@@ -97,18 +98,18 @@ export class UserProfile extends Component<{ store?: any }, { show: boolean }> {
             >
               {({ handleSubmit, handleChange, values: userInputs, errors }) => (
                 <Form
-                  className='user-profile'
+                  className="user-profile"
                   noValidate
                   onSubmit={handleSubmit}
                 >
                   <Form.Row>
                     <Col md={4}>
                       <Form.Group
-                        className='image-container'
+                        className="image-container"
                         style={{ border: '2px solid #4e5de5' }}
                       >
                         <Image
-                          className='profile-img'
+                          className="profile-img"
                           src={this.props.store.currentUser.photoUrl}
                           // fluid
                         />
@@ -116,14 +117,14 @@ export class UserProfile extends Component<{ store?: any }, { show: boolean }> {
                       <Form.Group>
                         <Form.Label>Photo</Form.Label>
                         <Form.Control
-                          name='photoUrl'
-                          type='text'
-                          placeholder='Enter photo URL'
+                          name="photoUrl"
+                          type="text"
+                          placeholder="Enter photo URL"
                           value={userInputs.photoUrl}
                           onChange={handleChange}
                           isInvalid={!!errors.photoUrl}
                         />
-                        <Form.Control.Feedback type='invalid'>
+                        <Form.Control.Feedback type="invalid">
                           {errors.photoUrl}
                         </Form.Control.Feedback>
                       </Form.Group>
@@ -133,70 +134,70 @@ export class UserProfile extends Component<{ store?: any }, { show: boolean }> {
                       <Form.Group>
                         <Form.Label>First name</Form.Label>
                         <Form.Control
-                          name='firstName'
-                          type='text'
-                          placeholder='Enter your first name'
+                          name="firstName"
+                          type="text"
+                          placeholder="Enter your first name"
                           value={userInputs.firstName}
                           onChange={handleChange}
                           isInvalid={!!errors.firstName}
                         />
-                        <Form.Control.Feedback type='invalid'>
+                        <Form.Control.Feedback type="invalid">
                           {errors.firstName}
                         </Form.Control.Feedback>
                       </Form.Group>
                       <Form.Group>
                         <Form.Label>Last name</Form.Label>
                         <Form.Control
-                          name='lastName'
-                          type='text'
-                          placeholder='Enter your last name'
+                          name="lastName"
+                          type="text"
+                          placeholder="Enter your last name"
                           value={userInputs.lastName}
                           onChange={handleChange}
                           isInvalid={!!errors.lastName}
                         />
-                        <Form.Control.Feedback type='invalid'>
+                        <Form.Control.Feedback type="invalid">
                           {errors.lastName}
                         </Form.Control.Feedback>
                       </Form.Group>
                       <Form.Group>
                         <Form.Label>Nickname</Form.Label>
                         <Form.Control
-                          name='nickname'
-                          type='text'
-                          placeholder='Enter your nickname'
+                          name="nickname"
+                          type="text"
+                          placeholder="Enter your nickname"
                           value={userInputs.nickname}
                           onChange={handleChange}
                           isInvalid={!!errors.nickname}
                         />
-                        <Form.Control.Feedback type='invalid'>
+                        <Form.Control.Feedback type="invalid">
                           {errors.nickname}
                         </Form.Control.Feedback>
                       </Form.Group>
                       <Form.Group>
                         <Form.Label>Email</Form.Label>
                         <Form.Control
-                          name='email'
-                          type='email'
-                          placeholder='Enter your email'
+                          name="email"
+                          type="email"
+                          placeholder="Enter your email"
                           value={userInputs.email}
                           onChange={handleChange}
                           isInvalid={!!errors.email}
                         />
-                        <Form.Control.Feedback type='invalid'>
+                        <Form.Control.Feedback type="invalid">
                           {errors.email}
                         </Form.Control.Feedback>
                       </Form.Group>
                       <Form.Group>
                         <Form.Label>Password</Form.Label>
                         <Form.Control
-                          name='password'
-                          type='password'
-                          placeholder='Enter your password'
+                          name="password"
+                          type="password"
+                          placeholder="Enter your password"
                           value={userInputs.password}
                           onChange={handleChange}
                           isInvalid={!!errors.password}
                         />
-                        <Form.Control.Feedback type='invalid'>
+                        <Form.Control.Feedback type="invalid">
                           {errors.password}
                         </Form.Control.Feedback>
                       </Form.Group>
@@ -206,70 +207,70 @@ export class UserProfile extends Component<{ store?: any }, { show: boolean }> {
                       <Form.Group>
                         <Form.Label>City</Form.Label>
                         <Form.Control
-                          name='city'
-                          type='text'
-                          placeholder='Enter your city'
+                          name="city"
+                          type="text"
+                          placeholder="Enter your city"
                           value={userInputs.city}
                           onChange={handleChange}
                           isInvalid={!!errors.city}
                         />
-                        <Form.Control.Feedback type='invalid'>
+                        <Form.Control.Feedback type="invalid">
                           {errors.city}
                         </Form.Control.Feedback>
                       </Form.Group>
                       <Form.Group>
                         <Form.Label>Postal code</Form.Label>
                         <Form.Control
-                          name='postalCode'
-                          type='text'
-                          placeholder='Enter your postal code'
+                          name="postalCode"
+                          type="text"
+                          placeholder="Enter your postal code"
                           value={userInputs.postalCode}
                           onChange={handleChange}
                           isInvalid={!!errors.postalCode}
                         />
-                        <Form.Control.Feedback type='invalid'>
+                        <Form.Control.Feedback type="invalid">
                           {errors.postalCode}
                         </Form.Control.Feedback>
                       </Form.Group>
                       <Form.Group>
                         <Form.Label>Street</Form.Label>
                         <Form.Control
-                          name='street'
-                          type='text'
-                          placeholder='Enter your street'
+                          name="street"
+                          type="text"
+                          placeholder="Enter your street"
                           value={userInputs.street}
                           onChange={handleChange}
                           isInvalid={!!errors.street}
                         />
-                        <Form.Control.Feedback type='invalid'>
+                        <Form.Control.Feedback type="invalid">
                           {errors.street}
                         </Form.Control.Feedback>
                       </Form.Group>
                       <Form.Group>
                         <Form.Label>House</Form.Label>
                         <Form.Control
-                          name='house'
-                          type='text'
-                          placeholder='Enter your house'
+                          name="house"
+                          type="text"
+                          placeholder="Enter your house"
                           value={userInputs.house}
                           onChange={handleChange}
                           isInvalid={!!errors.house}
                         />
-                        <Form.Control.Feedback type='invalid'>
+                        <Form.Control.Feedback type="invalid">
                           {errors.house}
                         </Form.Control.Feedback>
                       </Form.Group>
                       <Form.Group>
                         <Form.Label>Apartment</Form.Label>
                         <Form.Control
-                          name='apartment'
-                          type='text'
-                          placeholder='Enter your apartment'
+                          name="apartment"
+                          type="text"
+                          placeholder="Enter your apartment"
                           value={userInputs.apartment}
                           onChange={handleChange}
                           isInvalid={!!errors.apartment}
                         />
-                        <Form.Control.Feedback type='invalid'>
+                        <Form.Control.Feedback type="invalid">
                           {errors.apartment}
                         </Form.Control.Feedback>
                       </Form.Group>
@@ -277,20 +278,20 @@ export class UserProfile extends Component<{ store?: any }, { show: boolean }> {
                       <Form.Group>
                         <Form.Label>Phone</Form.Label>
                         <Form.Control
-                          name='phone'
-                          type='text'
-                          placeholder='Enter your phone'
+                          name="phone"
+                          type="text"
+                          placeholder="Enter your phone"
                           value={userInputs.phone}
                           onChange={handleChange}
                           isInvalid={!!errors.phone}
                         />
-                        <Form.Control.Feedback type='invalid'>
+                        <Form.Control.Feedback type="invalid">
                           {errors.phone}
                         </Form.Control.Feedback>
                       </Form.Group>
                     </Col>
                   </Form.Row>
-                  <Button variant='primary' type='submit'>
+                  <Button variant="primary" type="submit">
                     Save changes
                   </Button>
                 </Form>
