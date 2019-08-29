b1f43c3e69dcf3d371d31d9d481827eb25e63084
unknown <lbajdull@gmail.com>
2019-08-29 20:07:04 +0300

    add signout button to MainPage

--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -9,6 +9,7 @@ import { TeamService } from '../../services/TeamService';
 import './MainPage.css';
 import avengers from '../../photos/avengers.jpg';
 import { UserProfile } from '../../components/UserProfile/UserProfile';
+import { Button } from 'react-bootstrap';
 
 @inject('store')
 @observer
@@ -38,14 +39,20 @@ export class MainPage extends Component<
   async componentDidMount() {
     const teamsList = await this.teamService.fetchFunc('GET', 'api/teams');
     this.setState({ allTeamsList: teamsList });
+    // if (localStorage.getItem('token')) {
+    //   this.props.store.isLoggedIn = true;
+    // }
   }
 
   render() {
     let authorisationSection;
     if (this.props.store.isLoggedIn) {
       authorisationSection = (
-        <div className="profile-tab">
-          <div className="greeting-tab">
+        <div className='profile-tab'>
+          <Button variant='danger' onClick={this.props.store.signOut}>
+            Sign out
+          </Button>
+          <div className='greeting-tab'>
             Hello, {this.props.store.currentUser.firstName}
           </div>
           <UserProfile />
@@ -53,33 +60,41 @@ export class MainPage extends Component<
       );
     } else {
       authorisationSection = (
-        <div>
+        <div className='profile-tab'>
           <RegistrationForm /> or <LoginForm />
         </div>
       );
     }
 
     return (
-      <div className="mainpage-container">
-        <div className="content-wrap">
-          <header className="header">
-            <nav className="search-input">
+      <div className='mainpage-container'>
+        <div className='content-wrap'>
+          <header className='header'>
+            <nav className='search-input'>
               <input />
             </nav>
-            <div className="app-title">ATHLETE TRACKER</div>
+            <div className='app-title'>ATHLETE TRACKER</div>
             {authorisationSection}
           </header>
-          <main className="teamcards-list-container">
+          <main className='teamcards-list-container'>
             <h2>All teams: {this.state.allTeamsList.length}</h2>
-            <div className="all-teamcards-list">
+            <div className='all-teamcards-list'>
               {this.state.allTeamsList.map(
                 (item: {
                   name: string;
                   photoUrl: string;
                   id: string;
                   userTeams: Array<any>;
-                }) => (
-                  <>
+                }) => {
+                  let isMember;
+                  if (this.props.store.userTeams.includes(item.id)) {
+                    isMember = true;
+                  } else {
+                    isMember = false;
+                  }
+                  // if item.id is one of values in userTeams[] in store
+                  // set prop isMember=true, else false
+                  return (
                     <TeamCard
                       name={item.name}
                       photoUrl={item.photoUrl}
@@ -87,15 +102,15 @@ export class MainPage extends Component<
                       memberCount={
                         item.userTeams ? item.userTeams.length : undefined
                       }
+                      isMember={isMember}
                     />
-                    {/* <div>{item.userTeams}</div> */}
-                  </>
-                )
+                  );
+                }
               )}
             </div>
           </main>
         </div>
-        <footer className="footer">Footer</footer>
+        <footer className='footer'>Footer</footer>
       </div>
     );
   }
