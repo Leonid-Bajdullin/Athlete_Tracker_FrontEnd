f25590df75306071df4a760ea0f074f0f15bad71
unknown <lbajdull@gmail.com>
2019-08-30 18:52:38 +0300

    inject mobx store in TeamCard for funcionality

--- src/components/TeamCard/TeamCard.tsx
+++ src/components/TeamCard/TeamCard.tsx
@@ -1,40 +1,64 @@
-import React from 'react';
+import React, { Props } from 'react';
 import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { inject, observer } from 'mobx-react';
 
 import './TeamCard.css';
-import { Link } from 'react-router-dom';
 
-export function TeamCard(props: {
-  name: string;
-  photoUrl: string;
-  id: string;
-  memberCount?: number;
-  isMember: boolean;
-}) {
-  let buttonView = null;
-  if (props.isMember) {
-    buttonView = (
-      <Link to={`/teamprofile/${props.id}`}>
-        <Button className='button-div'>View</Button>
-      </Link>
-    );
-  } else {
-    buttonView = null;
-  }
+export const TeamCard = inject('store')(
+  observer(
+    (props: {
+      name: string;
+      photoUrl: string;
+      teamId: string;
+      memberCount?: number;
+      isMember: boolean;
+      userId: string;
+      position?: string;
+      store?: any;
+    }) => {
+      const joinTeam = async () => {
+        const values = {
+          userId: props.store.currentUser.id,
+          teamId: props.teamId
+        };
+        props.store.joinTeamRequest(values);
+      };
 
-  return (
-    <section style={{ width: '15rem', height: '20rem' }} className='team-card'>
-      <div className='team-photo'>
-        <img alt='Team Photo' src={props.photoUrl} />
-      </div>
-      <div className='team-info'>
-        <div className='title'>{props.name}</div>
-        <div className='description'>Members count: {props.memberCount}</div>
-        {buttonView}
-        {/* <Link to={`/teamprofile/${props.id}`}>
-          <Button className='button-div'>View</Button>
-        </Link> */}
-      </div>
-    </section>
-  );
-}
+      let buttonView = null;
+      if (props.isMember) {
+        buttonView = (
+          <Link to={`/teamprofile/${props.teamId}`}>
+            <Button variant='primary' className='button-div'>
+              View
+            </Button>
+          </Link>
+        );
+      } else {
+        buttonView = (
+          <Button variant='success' className='button-div' onClick={joinTeam}>
+            Join
+          </Button>
+        );
+      }
+
+      return (
+        <section
+          style={{ width: '15rem', height: '20rem' }}
+          className='team-card'
+        >
+          <div className='team-photo'>
+            <img alt='Team Photo' src={props.photoUrl} />
+          </div>
+          <div className='team-info'>
+            <div className='title'>{props.name}</div>
+            <div className='description'>
+              Members count: {props.memberCount}
+            </div>
+            {buttonView}
+          </div>
+        </section>
+      );
+    }
+  )
+);
