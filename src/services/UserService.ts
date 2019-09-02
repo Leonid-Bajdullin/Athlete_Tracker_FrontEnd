116d92acc3b0037a7d366b772525eba8aa18ff51
Leo <lbajdull@gmail.com>
2019-09-03 00:36:00 +0300

    delete catch block from UserService method

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -4,12 +4,8 @@ import { BaseService } from './BaseService';
 
 @Service()
 export class UserService extends BaseService {
-  public submitRegistration = async (
-    values: any
-  ): Promise<{ user: any; token: string }> => {
-    return this.fetchFunc('POST', 'api/users', values).catch((result) =>
-      alert(result.message)
-    );
+  public submitRegistration = async (values: any): Promise<any> => {
+    return this.fetchFunc('POST', 'api/users', values);
   };
 
   public submitLogin = async (values: any): Promise<any> => {
