5c5e682cd112086ded4dc58369b0960070b86917
Leo <lbajdull@gmail.com>
2019-09-06 11:43:46 +0300

    wip

--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -38,7 +38,6 @@ export class MainPage extends Component<
   };
 
   async componentDidMount() {
-    debugger;
     await this.checkStorage();
     const teamsList = await this.teamService.fetchFunc('GET', 'api/teams');
     this.setState({ allTeamsList: teamsList });
@@ -50,19 +49,19 @@ export class MainPage extends Component<
     let authorisationSection;
     if (this.props.store.isLoggedIn) {
       authorisationSection = (
-        <div className='profile-tab'>
-          <Button variant='danger' onClick={this.props.store.signOut}>
+        <div className="profile-tab">
+          <Button variant="danger" onClick={this.props.store.signOut}>
             Sign out
           </Button>
-          <div className='greeting-tab'>Hello, {currentUser.firstName}</div>
+          <div className="greeting-tab">Hello, {currentUser.firstName}</div>
           <UserProfile />
         </div>
       );
     } else {
       authorisationSection = (
-        <div className='profile-tab'>
+        <div className="profile-tab">
           <RegistrationForm />
-          <div className='greeting-tab'>
+          <div className="greeting-tab">
             If you already
             <br /> registered >>>
           </div>
@@ -72,20 +71,20 @@ export class MainPage extends Component<
     }
 
     return (
-      <div className='mainpage-container'>
-        <div className='content-wrap'>
-          <header className='header'>
-            <nav className='search-input'>
+      <div className="mainpage-container">
+        <div className="content-wrap">
+          <header className="header">
+            <nav className="search-input">
               <input />
             </nav>
-            <div className='app-title'>
-              <Link to='/'>ATHLETE TRACKER</Link>
+            <div className="app-title">
+              <Link to="/">ATHLETE TRACKER</Link>
             </div>
             {authorisationSection}
           </header>
-          <main className='teamcards-list-container'>
+          <main className="teamcards-list-container">
             <h2>All teams: {this.state.allTeamsList.length}</h2>
-            <div className='all-teamcards-list'>
+            <div className="all-teamcards-list">
               <TeamCreateForm />
               {this.state.allTeamsList.map(
                 (item: {
@@ -120,7 +119,7 @@ export class MainPage extends Component<
             </div>
           </main>
         </div>
-        <footer className='footer'>Copyright© Leo Peo, 2019</footer>
+        <footer className="footer">Copyright© Leo Peo, 2019</footer>
       </div>
     );
   }
