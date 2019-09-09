6f22354a772a438b5f5c5815b61346dbe002162a
unknown <lbajdull@gmail.com>
2019-09-09 15:57:06 +0300

    use Definite Assignment Assertions for DTO

--- src/dto/UserCardPropsDto.ts
+++ src/dto/UserCardPropsDto.ts
@@ -1,23 +1,11 @@
 export class UserCardPropsDto {
-  constructor() {
-    // this.fullName = '';
-    // this.photoUrl = '';
-    this.id = '';
-    this.teamId = '';
-    this.email = '';
-    this.position = '';
-    this.currUserPos = '';
-    this.onItemChange = '';
-    this.store = '';
-  }
-
-  public fullName: string = '';
+  public fullName!: string;
   public photoUrl!: string;
-  public id: string;
-  public teamId: string;
-  public email: string;
-  public position: string;
-  public currUserPos: string;
-  public onItemChange: any;
+  public id!: string;
+  public teamId!: string;
+  public email!: string;
+  public position!: string;
+  public currUserPos!: string;
+  public onItemChange!: any;
   public store?: any;
 }
