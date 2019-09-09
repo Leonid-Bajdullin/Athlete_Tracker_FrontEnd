fd3156542c7d98d3525fe9dc14b931c7b7792bcc
Leo <lbajdull@gmail.com>
2019-09-09 09:04:44 +0300

    add checking for token in fetchFunc

--- src/services/BaseService.ts
+++ src/services/BaseService.ts
@@ -1,32 +1,37 @@
-// prettier-ignore
-export abstract class BaseService {
-  public addAuthHeaderMiddleware = () => {
-    return localStorage.getItem("token");
-  }
+import history from 'history';
 
+export abstract class BaseService {
+  public addAuthHeader = (obj: any) => {
+    let token = localStorage.getItem('token');
+    if (token) {
+      obj.headers.Authorization = `Bearer ${token}`;
+    }
+  };
+
+  // prettier-ignore
   public fetchFunc(method: string, route: string, data?: {}) {
-    // debugger;
-    const authorization = this.addAuthHeaderMiddleware();
     let obj = {
       headers: {
         'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${authorization}`
+        'Content-Type': 'application/json'
       },
       method: method
     } as any;
+    this.addAuthHeader(obj);
     if (method === 'POST' || 'PUT') {
       obj.body = JSON.stringify(data);
     }
-    return fetch(`http://localhost:4000/${route}`, obj).then((res) =>
-      {
-        if (res.status !== 200) {
-          return res.json().then(data => {
-            throw new Error(data.message);
-          });          
-        }
-        return res.json();
+    return fetch(`http://localhost:4000/${route}`, obj).then((res) => {
+      if (res.status === 401) {
+        window.location.href = '/';
+        throw new Error('Please log in');
       }
-    );
+      if (res.status !== 200) {
+        return res.json().then((data) => {
+          throw new Error(data.message);
+        });
+      }
+      return res.json();
+    });
   }
 }
